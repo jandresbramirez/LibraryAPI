3b5baea229b08faf296eb2978cecc2e78f06113f
JBAndrés_ <147890031+jandresbramirez@users.noreply.github.com>
2025-09-11 00:10:42 +0000

    Creación repositorio de usuarios

--- repositories/book_repository.py
+++ repositories/book_repository.py
@@ -11,15 +11,19 @@ class BookRepository:
     def __init__(self, db_session: Session):
         self.db = db_session
 
+    #Obtener todos los libros de la base de datos
     def get_all_books(self):
         return self.db.query(Book).all()
 
+    #Obtener un libro en específico por un ID
     def get_book_by_id(self, book_id: int):
         return self.db.query(Book).filter(Book.id = book_id).first()
 
+    #Obtener un libro en específico por un Autor
     def get_books_by_author(self, author_id: int):
         return self.db.query(Book).filter(Book.author_id = author_id).all()
 
+    #Crear un nuevo prestamo
     def create_book(self, title: str, author_id: int):
         new_book = Book(title=title, author_id=author_id)
         self.db.add(new_book)
@@ -27,6 +31,7 @@ class BookRepository:
         self.db.refresh(new_book)
         return new_book
 
+    #Actualizar titulo y/o autor de un libro por un ID
     def update_book(self, book_id: int, title: str = None, author_id: int = None):
         book = self.get_book_by_id(book_id)
         if book:
@@ -38,6 +43,7 @@ class BookRepository:
             self.db.refresh(book)
         return book
 
+    #Eliminar un libro por su ID
     def delete_book(self, book_id: int):
         book = self.get_book_by_id(book_id)
         if book:
---
+++ repositories/user_repository.py
@@ -0,0 +1,53 @@
+from sqlalchemy.orm import Session
+from models.library_model import User
+
+class UserRepository:
+
+    """
+    Repositorio para la gestión de libros en la base de datos
+    integrando un CRUD.
+    """
+
+    def __init__(self, db_session: Session):
+        self.db = db_session
+
+    #Obtener todos los usuarios de la base de datos
+    def get_all_users(self):
+        return self.db.query(User).all()
+
+    #Obtener un usuario en específico por un ID
+    def get_user_by_id(self, user_id: int):
+        return self.db.query(User).filter(User.id == user_id).first()
+
+    #Obtener un usuario en específico por email
+    def get_user_by_email(self, email: str):
+        return self.db.query(User).filter(User.email == email).first()
+
+    #Crear un nuevo usuario
+    def create_user(self, name: str, email: str):
+        new_user = User(name = name, email = email)
+        self.db.add(new_user)
+        self.db.commit()
+        self.db.refresh(new_user)
+        return new_user
+
+    #Actualizar nombre y/o correo de un usuario por su ID.
+    def update_user(self, user_id: int, name: str = None, email: str = None):
+        user = self.get_user_by_id(user_id)
+        if user:
+            if name:
+                user.name = name
+            if email:
+                user.email = email
+            self.db.commit()
+            self.db.refresh(user)
+        return user
+
+    #Eliminar un usuario por su ID
+    def delete_user(self, user_id: int):
+        user = self.get_user_by_id(user_id)
+        if user:
+            self.db.delete(user)
+            self.db.commit()
+        return user
+
