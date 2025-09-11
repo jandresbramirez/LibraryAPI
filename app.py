f1a30c7d7418dd6994eea81b4364402aafd299be
JBAndrés_ <147890031+jandresbramirez@users.noreply.github.com>
2025-09-11 19:23:03 +0000

    Correción básica

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ app.register_blueprint(book_bp)
 app.register_blueprint(user_bp)
 
 if __name__ == "__main__":
-    app.run(host"0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
