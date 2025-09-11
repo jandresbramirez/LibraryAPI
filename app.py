d3d69b936b126dfb43053efb465eb50e796cac55
JBAndrés_ <147890031+jandresbramirez@users.noreply.github.com>
2025-09-11 19:21:28 +0000

    Importaciones a main y registro de blueprint

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from controllers.author_controller import author_bp
+from controllers.loan_controller import loan_bp
+from controllers.book_controller import book_bp
+from controllers.user_controller import user_bp
 
 app = Flask(__name__)
 
+app.register_blueprint(author_bp)
+app.register_blueprint(loan_bp)
+app.register_blueprint(book_bp)
+app.register_blueprint(user_bp)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host"0.0.0.0", port=5000, debug=True)
